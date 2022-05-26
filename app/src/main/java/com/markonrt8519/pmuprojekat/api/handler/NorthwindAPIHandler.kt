dc73a07ed446c53ea810cf16b7cb9402bcfbcc7a
Marko Dujović <dujovicm@outlook.com>
2022-05-26 14:52:07 +0200

    REST API Methods

--- app/src/main/java/com/markonrt8519/pmuprojekat/api/handler/NorthwindAPIHandler.kt
+++ app/src/main/java/com/markonrt8519/pmuprojekat/api/handler/NorthwindAPIHandler.kt
@@ -1,12 +1,17 @@
 package com.markonrt8519.pmuprojekat.api.handler
 
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
 import java.net.URL
 
 class NorthwindAPIHandler {
     val CLIENT = OkHttpClient()
     val BASE_URL = "http://94.156.189.137/api/"
+    val MEDIA_TYPE = "application/json; charset=utf-8".toMediaTypeOrNull()
 
     fun getRequest(sUrl: String): String? {
         var result: String? = null
@@ -17,9 +22,65 @@ class NorthwindAPIHandler {
             result = response.body?.string()
         }
         catch (err: Error) {
-            print("Error when executing get request: " + err.localizedMessage)
+            print("Error when executing GET request: " + err.localizedMessage)
         }
 
+        return result
+    }
+
+    fun postRequest(sUrl: String,data:String):String?{
+        var result: String? = null
+        try {
+            // Create URL
+            val url = URL(BASE_URL+sUrl)   // Build request
+            val body=data.toRequestBody(MEDIA_TYPE)
+            val request = Request.Builder()
+                .url(url)
+                .post(body)
+                .build()   // Execute request
+            val response = CLIENT.newCall(request).execute()
+            result = response.body?.string()
+        }
+        catch(err:Error) {
+            print("Error when executing POST request: "+err.localizedMessage)
+        }
+        return result
+    }
+
+    fun putRequest(sUrl: String,data:String):String?{
+        var result: String? = null
+        try {
+            // Create URL
+            val url = URL(BASE_URL+sUrl)   // Build request
+            val body=data.toRequestBody(MEDIA_TYPE)
+            val request = Request.Builder()
+                .url(url)
+                .put(body)
+                .build()   // Execute request
+            val response = CLIENT.newCall(request).execute()
+            result = response.body?.string()
+        }
+        catch(err:Error) {
+            print("Error when executing PUT request: "+err.localizedMessage)
+        }
+        return result
+    }
+
+    fun deleteRequest(sUrl: String):String?{
+        var result: String? = null
+        try {
+            // Create URL
+            val url = URL(BASE_URL+sUrl)   // Build request
+            val request = Request.Builder()
+                .url(url)
+                .delete()
+                .build()   // Execute request
+            val response = CLIENT.newCall(request).execute()
+            result = response.body?.string()
+        }
+        catch(err:Error) {
+            print("Error when executing DELETE request: "+err.localizedMessage)
+        }
         return result
     }
 }
