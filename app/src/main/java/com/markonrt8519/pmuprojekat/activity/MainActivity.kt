25cd4411819e35fb42b7f494950060668404c683
Marko Dujović <dujovicm@outlook.com>
2022-05-27 00:14:31 +0200

    Refurbish main activity

--- app/src/main/java/com/markonrt8519/pmuprojekat/activity/MainActivity.kt
+++ app/src/main/java/com/markonrt8519/pmuprojekat/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.markonrt8519.pmuprojekat.activity
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
